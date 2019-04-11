4c2d150b292393c5ee27adc097302322c21df8fe
DESKTOP-NA4SBTF\suzuk <suzukikaito14.ne.jp@gmail.com>
2019-04-11 22:21:38 +0900

    update Exercise2.5

--- Chapter2_Multi-armed_Bandits/solver.py
+++ Chapter2_Multi-armed_Bandits/solver.py
@@ -7,11 +7,12 @@ def epsilon_greedy(problem, n_steps, epsilon, initial_value=0.0, step_size=-1):
     n_states = problem.n_states
     pred_q_table = initial_value * np.ones((n_states, k_arms))
 
+    optimal_action_count = 0
+    optimal_action_rates = np.zeros(n_steps)
     cumulative_reward = 0
-    selected_actions = np.zeros(n_steps)
     average_rewards = np.zeros(n_steps)
 
-    state = 0
+    state = 0 # 初期状態が0なのは知っていることにする
     for step in range(n_steps):
         is_greedy = np.random.rand() >= epsilon
         if is_greedy:
@@ -22,8 +23,9 @@ def epsilon_greedy(problem, n_steps, epsilon, initial_value=0.0, step_size=-1):
             # 全ての行動
             action_candidates = np.arange(k_arms)
         action = np.random.choice(action_candidates)
+        optimal_action_count += (action == problem._get_optimal_action())
 
-        reward, state = problem.try_an_arm(action)
+        reward, next_state = problem.try_an_arm(action)
         cumulative_reward += reward
 
         if step_size == -1:
@@ -31,11 +33,12 @@ def epsilon_greedy(problem, n_steps, epsilon, initial_value=0.0, step_size=-1):
         else:
             alpha = step_size
         pred_q_table[state][action] += alpha * (reward - pred_q_table[state][action])
+        state = next_state
 
-        selected_actions[step] = action
+        optimal_action_rates[step] = optimal_action_count / (step+1)
         average_rewards[step] = cumulative_reward / (step+1)
 
-        return pred_table, selected_actions, average_rewards
+    return pred_q_table, optimal_action_rates, average_rewards
 
 
 def UCB1(problem, n_steps, c=1.0):
--- Chapter2_Multi-armed_Bandits/visualize.py
+++
@@ -1,5 +0,0 @@
-def show_average_reward():
-    1+1
-
-def show_optimal_action_rate():
-    1+1
