34cfb3a7b6fb3d452983e398f003a65a29ccf8df
DESKTOP-NA4SBTF\suzuk <suzukikaito14.ne.jp@gmail.com>
2019-04-14 19:43:01 +0900

    update problem.py

--- Chapter2_Multi-armed_Bandits/problem.py
+++ Chapter2_Multi-armed_Bandits/problem.py
@@ -1,20 +1,23 @@
 import numpy as np
 
 
+# [Stationary|NonStationary]BanditProblemのための抽象クラス
 class BanditProblem:
     def __init__(self, k_arms, reward_std):
         self.k_arms = k_arms
         self.n_states = 1
-        self._true_qs = None
+        self._true_qs = None  # 真の価値
         self._reward_std = reward_std
+        # ContextualBanditProblemとの整合性のためself._stateも持っておく（常に0）
+        self._state = 0
 
     def try_an_arm(self, k):
         if self._true_qs is None:
             raise NotImplementedError
         true_q = self._true_qs[k]
+        # 報酬は真の価値を平均とし, reward_stdを分散とした正規分布からサンプルされる
         reward = np.random.normal(true_q, self._reward_std)
-        # ContextualBanditProblemとの整合性のためstate(always 0)も返す
-        return reward, 0
+        return reward, self._state
 
     def _get_optimal_action(self):
         if self._true_qs is None:
@@ -22,12 +25,16 @@ class BanditProblem:
         return np.argmax(self._true_qs)
 
 
+# 定常なバンディット問題
+# 初めに平均0, 標準偏差problem_stdで生成した価値をずっと使う
 class StationaryBanditProblem(BanditProblem):
     def __init__(self, k_arms, reward_std=1.0, problem_std=1.0):
         super().__init__(k_arms, reward_std)
         self._true_qs = np.random.normal(0, problem_std, k_arms)
 
 
+# 非定常なバンディット問題
+# 初めに価値を0で初期化し, 以後毎ステップ平均0, 標準偏差change_stdが加算されて変化する
 class NonStationaryBanditProblem(BanditProblem):
     def __init__(self, k_arms, reward_std=1.0, change_std=0.01):
         super().__init__(k_arms, reward_std)
@@ -38,22 +45,25 @@ class NonStationaryBanditProblem(BanditProblem):
         true_q = self._true_qs[k]
         reward = np.random.normal(true_q, self._reward_std)
         self._true_qs += np.random.normal(0, self._change_std, self.k_arms)
-        return reward, 0
+        return reward, self._state
 
 
+# 文脈付きバンディット問題
+# 内部にn_states個のStationaryBanditProblemを持ち, ランダムにその中の1つが状態として選ばれる
+# state_infoによって状態の情報を返すかを選べる. Falseなら常に0が返る
 class ContextualBanditProblem:
     def __init__(self, k_arms, reward_std=1.0, problem_std=1.0,
                  n_states=2, state_info=True):
         self.k_arms = k_arms
         self._reward_std = reward_std
-        self.problems = [StationaryBanditProblem(k_arms, reward_std, problem_std)
-                         for _ in range(n_states)]
+        self._problems = [StationaryBanditProblem(k_arms, reward_std, problem_std)
+                          for _ in range(n_states)]
         self.n_states = n_states
         self.state_info = state_info
-        self._state = 0 # 初期状態は0
+        self._state = 0  # 初期状態は0
 
     def try_an_arm(self, k):
-        problem = self.problems[self._state]
+        problem = self._problems[self._state]
         reward, _ = problem.try_an_arm(k)
         next_state = np.random.randint(self.n_states)
         self._state = next_state
@@ -63,4 +73,4 @@ class ContextualBanditProblem:
             return reward, 0
 
     def _get_optimal_action(self, state):
-        return self.problems[state]._get_optimal_action()
+        return self._problems[state]._get_optimal_action()
--- Chapter2_Multi-armed_Bandits/solver.py
+++ Chapter2_Multi-armed_Bandits/solver.py
@@ -1,7 +1,98 @@
 import numpy as np
 
 
+class EpsilonGreedy:
+    # step_sizeはデフォルトで-1で, この状態だと(1/step数)を学習率に用いる
+    def __init__(self, n_states, k_arms, initial_value=0.0, epsilon=0.1, step_size=-1):
+        self.n_states = n_states
+        self.k_arms = k_arms
+        self.pred_q_table = initial_value * np.ones((n_states, k_arms))
+        self.epsilon = epsilon
+        self.step_size = step_size
+        self.step_count = 0
+
+    def __call__(self, state):
+        self.step_count += 1
+        is_greedy = np.random.rand() >= self.epsilon
+        if is_greedy:
+            # 行動価値が等しい行動が複数あるときは, その中からランダムに選ぶ
+            pred_qs = self.pred_q_table[state]
+            action_candidates = np.arange(self.k_arms)[pred_qs == np.max(pred_qs)]
+        else:
+            # 全ての行動からランダムに選ぶ
+            action_candidates = np.arange(self.k_arms)
+        action = np.random.choice(action_candidates)
+        return action
+
+    def update(self, state, action, reward):
+        if self.step_size == -1:
+            alpha = 1 / (1 + self.step_count)
+        else:
+            alpha = self.step_size
+
+        self.pred_q_table[state][action] += alpha * (reward - self.pred_q_table[state][action])
+
+
+class UCB1:
+    def __init__(self, n_states, k_arms, initial_value=0.0, conf_coeff=1.0):
+        self.n_states = n_states
+        self.k_arms = k_arms
+        self.pred_q_table = initial_value * np.ones((n_states, k_arms))
+        self.conf_coeff = conf_coeff
+        self.action_count = np.zeros(k_arms)
+
+    def __call__(self, state):
+        confidence = np.sqrt(np.log(np.sum(self.action_count)) / (self.action_count + 1e-6))
+        pred_qs = self.pred_q_table[state] + self.conf_coeff * confidence
+        action_candidates = np.arange(self.k_arms)[pred_qs == np.max(pred_qs)]
+        action = np.random.choice(action_candidates)
+        return action
+
+    def update(self, state, action, reward, alpha):
+        self.pred_q_table[state][action] += alpha * (reward - pred_q_table[state][action])
+
+
+class PolicyGradient:
+    def __init__(self, n_states, k_arms, initial_value):
+        1+1
+
+    def __call__(self, state):
+        raise NotImplementedError
+
+    def update(self, state, action, reward, alpha):
+        raise NotImplementedError
+
 # step_sizeはデフォルトで-1で, この状態だと(1/step数)を用いる.
+def run(problem, solver, n_steps, step_size=-1):
+    k_arms = problem.k_arms
+    n_states = problem.n_states
+
+    optimal_action_count = 0
+    optimal_action_rates = np.zeros(n_steps)
+    cumulative_reward = 0
+    average_rewards = np.zeros(n_steps)
+
+    state = 0 # 初期状態が0なのは知っていることにする
+    for step in range(n_steps):
+        action = solver(state)
+        optimal_action_count += (action == problem._get_optimal_action())
+
+        reward, next_state = problem.try_an_arm(action)
+        cumulative_reward += reward
+
+        if step_size == -1:
+            alpha = 1 / (step+1)
+        else:
+            alpha = step_size
+
+        solver.update(action, state, reward, alpha)
+        state = next_state
+
+        optimal_action_rates[step] = optimal_action_count / (step+1)
+        average_rewards[step] = cumulative_reward / (step+1)
+
+    return solver.pred_q_table, optimal_action_rates, average_rewards
+
 def epsilon_greedy(problem, n_steps, epsilon, initial_value=0.0, step_size=-1):
     k_arms = problem.k_arms
     n_states = problem.n_states
@@ -39,11 +130,4 @@ def epsilon_greedy(problem, n_steps, epsilon, initial_value=0.0, step_size=-1):
         average_rewards[step] = cumulative_reward / (step+1)
 
     return pred_q_table, optimal_action_rates, average_rewards
-
-
-def UCB1(problem, n_steps, c=1.0):
-    1+1
-
-def gradient_bandit(problem, n_steps, step_size):
-    1+1
 
